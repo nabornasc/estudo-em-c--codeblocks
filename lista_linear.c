548aba62695786196c4776ce9463b93236b0a08b
NaborNSIlva <nabornasc@gmail.com>
2026-09-24 19:27:09 -0400

    feat: adicionado arquivos vetores.c.h

---
+++ lista_linear.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializar(ListaSequencial *lista){
+    (*lista).tamanho=0;
+}
+
+
+
+#define TAM_MAX 100
---
+++ lista_linear.h
@@ -0,0 +1,13 @@
+#ifndef LISTA_LINEAR_H_INCLUDED
+#define LISTA_LINEAR_H_INCLUDED
+
+
+typedef struct {
+    int dados[TAM_MAX];
+    int tamanho;
+} ListaSequencial;
+
+
+
+
+#endif // LISTA_LINEAR_H_INCLUDED
